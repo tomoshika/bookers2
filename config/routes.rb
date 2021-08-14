0c217ed72dc17388efa123458a5433e35d6b26b9
tomoshika <tmm.orange.skm@gmail.com>
2021-08-14 09:27:09 +0000

    [Update]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'book/about' => 'homes#about'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show]
 end
