c818084346cd47cb6b120ec7c71503eecd9d5b34
tomoshika <tmm.orange.skm@gmail.com>
2021-08-17 14:17:58 +0000

    [Update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
 class BooksController < ApplicationController
   
   def new
-    @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.save
-    redirect_to books_path
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
   end
 
   def index
+    @book = Book.new
     @books = Book.all
     @user = current_user
   end
