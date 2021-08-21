d086d417b09385db4786935bc860fc4d4e2f4f6a
tomoshika <tmm.orange.skm@gmail.com>
2021-08-21 01:51:56 +0000

    [Update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,34 @@
 class UsersController < ApplicationController
-  
+
   def new
     @book = @user.book.new(book_params)
   end
-  
+
   def create
     @book = @user.book.new(book_params)
     @book.save
     redirect_to book_path(@book.id)
   end
-  
+
   def index
     @users = User.all
     @user = current_user
     @book = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
   end
-  
+
   def edit
     @user = User.find(params[:id])
+    unless @user == current_user
+      render :show
+    end
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -34,17 +37,17 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
   end
-  
-  
-  
-  
+
+
+
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
