7a73d0f779f96fbfbc52f628978bf9421aae11cd
tomoshika <tmm.orange.skm@gmail.com>
2021-08-27 05:45:08 +0000

    [Start]favorites

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,5 +5,10 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
+  has_many :favorites, dependent: :destory
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books
   resources :users
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
+  
 end
