c67f02ecf68aa19cf75ec657329cb5ab5d70116c
tomoshika <tmm.orange.skm@gmail.com>
2021-08-31 13:18:35 +0000

    [Start]4

--- db/migrate/20210831091715_add_index_favorites.rb
+++ db/migrate/20210831091715_add_index_favorites.rb
@@ -1,9 +1,9 @@
 class AddIndexFavorites < ActiveRecord::Migration[5.2]
   def change
     
-    # add_index :favorites, :user_id
-    # add_index :favorites, :book_id
-    # add_index :favorites, [:user_id, :book_id], unique: true
+    add_index :favorites, :user_id
+    add_index :favorites, :book_id
+    add_index :favorites, [:user_id, :book_id], unique: true
     
   end
 end
