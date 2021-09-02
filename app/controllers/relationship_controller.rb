8ea752199055351865bda0c962ec45a42efab88b
tomoshika <tmm.orange.skm@gmail.com>
2021-09-02 07:37:02 +0000

    [Update]4

---
+++ app/controllers/relationship_controller.rb
@@ -0,0 +1,9 @@
+class RelationshipController < ApplicationController
+  
+  def create
+  end
+  
+  def destroy
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
   has_many :books, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  has_many :post_comments, dependent: :destroy
-  attachment :profile_image
+  attachment :profile_image, destroy: false
 
-  validates :name, presence: true, uniqueness: true,
-                   length: { minimum: 2, maximum: 20 }
-
-  validates :introduction, length: { maximum: 50 }
-
-
+  validates :name, presence: true, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
 end
