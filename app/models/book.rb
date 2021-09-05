b47fc91ee6728cf0bbfdc7ecede6e68d7ba96b6f
tomoshika <tmm.orange.skm@gmail.com>
2021-09-05 07:28:21 +0000

    [Finish]5

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,18 +7,18 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
+
   def self.search(search, word)
     if search == "forward_match"
       @book = Book.where("title LIKE?","#{word}%")
     elsif search == "backward_match"
       @book = Book.where("title LIKE?","%#{word}")
     elsif search == "perfect_match"
-      @book = Book.where("#{word}")
+      @book = Book.where(name: word)
     elsif search == "partial_match"
       @book = Book.where("title LIKE?","%#{word}%")
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,22 @@ class User < ApplicationRecord
     reverse_of_relationships.find_by(following_id: user.id).present?
   end
    #フォローされてるかを判定する
-   
-  
+
+  def self.search(search,word)
+    if search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      @user = User.where(name: word)
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
+
 
 
 end
