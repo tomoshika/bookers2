b6440296d13ae4bb902c38385562d13253892a84
tomoshika <tmm.orange.skm@gmail.com>
2021-09-07 12:35:39 +0000

    [Update]6

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
+  has_many :favorites, dependent: :destroy
 
   has_many :relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :follower
