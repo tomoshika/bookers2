fb0216d67373c91a263aee888da29e228f0e75d2
tomoshika <tmm.orange.skm@gmail.com>
2021-09-09 06:55:20 +0000

    [Finish]6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,17 +1,17 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @post_comment = PostComment.new
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
     # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = book.post_comments.find(params[:id])
+    @book = Book.find(params[:book_id])
+    comment = @book.post_comments.find(params[:id])
 
    if current_user.id == comment.user.id
       comment.destroy
